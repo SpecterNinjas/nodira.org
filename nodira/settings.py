238f0f767e0e3c87214516c0329f7d5c72382be3
SpecterNinjas <47240330+SpecterNinjas@users.noreply.github.com>
2020-08-09 12:36:05 +0500

    Title Tag

--- nodira/settings.py
+++ nodira/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'nodira.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#        'default': {
-#           'ENGINE': 'django.db.backends.postgresql',
-#           'NAME': 'nodira.org',
-#           'USER': 'dadakhon',
-#           'PASSWORD': '1',
-#           'HOST': 'localhost',
-#           # 'PORT': '5432'
-#        }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+       'default': {
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'nodira.org',
+          'USER': 'dadakhon',
+          'PASSWORD': '1',
+          'HOST': 'localhost',
+          # 'PORT': '5432'
+       }
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
